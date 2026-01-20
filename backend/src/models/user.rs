0fb25e748658a449a582efb32b3694b4782df468
Cbailey371 <cbaileyh26@yahoo.com>
2026-01-20 17:37:00 -0500

    usurios

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -2,7 +2,7 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
-#[sea_orm(table_name = "Users")]
+#[sea_orm(table_name = "users")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
