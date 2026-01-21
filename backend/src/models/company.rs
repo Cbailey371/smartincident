7ef3cf57a5636a391b1311ae78ca388112a6bfb0
Cbailey371 <cbaileyh26@yahoo.com>
2026-01-20 19:06:03 -0500

    empresas

--- backend/src/models/company.rs
+++ backend/src/models/company.rs
@@ -2,7 +2,7 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
-#[sea_orm(table_name = "Companies")]
+#[sea_orm(table_name = "companies")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
